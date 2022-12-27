f6351724a97b7b87ebf9399de6eb5bda1db4f336
ManthanCitrusbug <100355413+ManthanCitrusbug@users.noreply.github.com>
2022-12-27 14:35:50 +0530

    Delete src directory

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.style.css']
-})
-
-export class AppComponent { }
